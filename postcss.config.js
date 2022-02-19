0ca71746c5a88637c6767a1daf4f2511e103052d
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2022-02-19 11:58:55 +0100

    initial setup

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    // require('@fullhuman/postcss-purgecss')({
+    //   content: ['./index.html', './projects.html', './src/main.ts'],
+    //   defaultExtractor: (content) => content.match(/[^<>"'`\s]*[^<>"'`\s:]/g),
+    // }),
+  ],
+};
