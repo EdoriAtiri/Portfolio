33fce84eb7f5196780ef8eb9b08b21d7b9c07a9d
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2022-04-27 12:54:27 +0100

    added transition animations for modal

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,10 +3,52 @@ import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 import { Projects } from '../data/projects';
 import { project } from '../interfaces/project';
 
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css'],
+  animations: [
+    trigger('toggleModal', [
+      state(
+        'show',
+        style({
+          opacity: 1,
+        })
+      ),
+      state(
+        'hide',
+        style({
+          opacity: 0,
+        })
+      ),
+      transition('show => hide', animate('300ms ease-out')),
+      transition('hide => show', animate('300ms ease-in')),
+    ]),
+    trigger('toggleModalBd', [
+      state(
+        'show',
+        style({
+          opacity: 0.25,
+        })
+      ),
+      state(
+        'hide',
+        style({
+          opacity: 0,
+        })
+      ),
+      transition('show => hide', animate('300ms 150ms ease-out')),
+      transition('hide => show', animate('300ms ease-in')),
+    ]),
+  ],
 })
 export class MainComponent implements OnInit {
   faArrowDown = faArrowDown;
