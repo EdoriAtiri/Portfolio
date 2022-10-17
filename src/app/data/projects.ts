213a6dcd20b803b5aa4508a69b554595d9080152
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2022-10-17 11:16:51 +0100

    added new projects to projects.ts, edited about section and header image overlay opacity

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -3,9 +3,20 @@ import { project } from '../interfaces/project';
 export const Projects: project[] = [
   {
     id: 1,
+    title: 'House Marketplace',
+    description:
+      'List your properties for sale or rent and purchase or rent properties. Built with React and Firebase',
+    desktopImage: '../../assets/images/house_marketplace.webp',
+    color: 'bg-emerald-200',
+    toggle: false,
+    live_url: 'https://prismatic-naiad-e0a94f.netlify.app/',
+    github_url: 'https://github.com/EdoriAtiri/House-Marketplace',
+  },
+  {
+    id: 2,
     title: 'Rest Countries',
     description:
-      'Discover interesting demographic facts about every single country',
+      'Discover interesting demographic facts about every single country. Built with Angular, TailwindCSS and Restcountries API',
     desktopImage: '../../assets/images/RestCountries-desktop.webp',
     color: 'bg-slate-200',
     toggle: false,
@@ -13,9 +24,21 @@ export const Projects: project[] = [
     github_url: 'https://github.com/EdoriAtiri/rest-countries',
   },
   {
-    id: 2,
+    id: 3,
+    title: 'Feedback UI',
+    description:
+      'User feedbacks for services provided. Created with React on the frontend and nodeJS and MongoDB on the backend',
+    desktopImage: '../../assets/images/feedbackUI.webp',
+    color: 'bg-fuchsia-200',
+    toggle: false,
+    live_url: 'https://wonderful-baklava-47c91a.netlify.app/',
+    github_url: 'https://github.com/EdoriAtiri/Feedback-app',
+  },
+  {
+    id: 4,
     title: 'Shortly',
-    description: 'Shorten lenghty URL links',
+    description:
+      'Shorten lenghty URL links. Built with Angular, Angular Material, SCSS and shrtcode API',
     desktopImage: '../../assets/images/shortly-desktop-.webp',
     color: 'bg-purple-200',
     toggle: false,
@@ -23,9 +46,10 @@ export const Projects: project[] = [
     github_url: 'https://github.com/EdoriAtiri/shortify',
   },
   {
-    id: 3,
+    id: 5,
     title: 'ImG Search',
-    description: 'Search free stock images - powered by the unsplash API',
+    description:
+      'Search free stock images - powered by the unsplash API. Built with HTML, CSS and Javascript',
     desktopImage: '../../assets/images/ImG Search-desktop.webp',
     color: 'bg-blue-200',
     toggle: false,
@@ -33,9 +57,10 @@ export const Projects: project[] = [
     github_url: 'https://github.com/EdoriAtiri/Image-Search',
   },
   {
-    id: 4,
+    id: 6,
     title: 'Fall Sneakers',
-    description: 'Sneaker product page',
+    description:
+      'Sneaker product page. Built with HTML, TailwindCSS and Javascript',
     desktopImage: '../../assets/images/E-commerce-desktop.webp',
     color: 'bg-orange-200',
     toggle: false,
@@ -43,9 +68,10 @@ export const Projects: project[] = [
     github_url: 'https://github.com/EdoriAtiri/E-commerce-product-page',
   },
   {
-    id: 5,
+    id: 7,
     title: 'Manage Landing Page',
-    description: 'Lead capture page for manage company',
+    description:
+      'Lead capture page for manage company. Built with HTML, CSS and JavaScript',
     desktopImage: '../../assets/images/Manage-landing-desktop.webp',
     color: 'bg-red-200',
     toggle: false,
